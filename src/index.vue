13b03692a433390242faba0c5bd2cfab402b3c8d
mkb <mk9007@163.com>
2023-06-22 02:38:04 +0800

    功能开发，异常调整

--- src/index.vue
+++ src/index.vue
@@ -18,8 +18,8 @@
 						open-type="getPhoneNumber" @getphonenumber="getPhoneNumber">
 						同意
 					</button>
-					<button v-if="openType === 'getUserInfo'" class="jw-button get-phone-number-button" open-type="getUserInfo"
-						@getuserinfo="getuserinfo">
+					<button v-if="openType === 'getUserInfo'" class="jw-button get-phone-number-button"
+						@click="getuserinfo">
 						同意
 					</button>
 				</slot>
@@ -29,56 +29,53 @@
 </template>
 
 <script>
-const silentLogin = function () {
-	return new Promise((resolve, reject) => {
-		uni.login({
-			success(res) {
-				resolve(res.code)
-			},
-			fail(err) {
-				reject(err)
-			}
+	const silentLogin = function() {
+		return new Promise((resolve, reject) => {
+			uni.login({
+				success(res) {
+					resolve(res.code)
+				},
+				fail(err) {
+					reject(err)
+				}
+			})
 		})
-	})
-}
-const getUserProfile = function () {
-	return new Promise((resolve, reject) => {
-		uni.getUserProfile({
-			lang: 'zh_CN',
-			desc: '用户登录',
-			success: (res) => {
-				resolve(res)
-			},
-			// 失败回调
-			fail: (err) => {
-				reject(err)
-			}
+	}
+	const getUserProfile = function() {
+		return new Promise((resolve, reject) => {
+			uni.getUserProfile({
+				lang: 'zh_CN',
+				desc: '用户登录',
+				success: (res) => {
+					resolve(res)
+				},
+				// 失败回调
+				fail: (err) => {
+					reject(err)
+				}
+			})
 		})
-	})
-};
+	};
 
-import props from './props.js';
-export default {
-	name: "jw-authorize",
-	mixins: [props],
-	data() {
-		return {
-			isShow: false,
-		};
-	},
-	methods: {
-		open() {
-			this.isShow = true;
+	import props from './props.js';
+	export default {
+		name: "jw-authorize",
+		mixins: [props],
+		data() {
+			return {
+				isShow: false,
+			};
 		},
-		close() {
-			this.isShow = false;
-		},
-		//获取用户信息授权
-		getuserinfo(e) {
-			let {
-				detail
-			} = e;
-			if (detail.errMsg == "getuserinfo:ok") {
+		methods: {
+			open() {
+				this.isShow = true;
+			},
+			close() {
+				this.isShow = false;
+			},
+			//获取用户信息授权
+			getuserinfo(e) {
+
 				let loginFunc = silentLogin();
 				let profile = getUserProfile();
 				loginFunc.then(code => {
@@ -90,7 +87,8 @@ export default {
 								code,
 								iv: res.iv,
 								encryptedData: res.encryptedData,
-								signature: res.signature
+								signature: res.signature,
+								rawData: res.rawData,
 							})
 						}).catch(err => {
 							reject(err)
@@ -99,7 +97,7 @@ export default {
 				}).then(meta => {
 					this.$emit("success", {
 						meta,
-						scene: 'getuserinfo'
+						scene: 'getuserinfo',
 					});
 				}).catch((err) => {
 					console.log(err)
@@ -108,147 +106,144 @@ export default {
 						icon: 'success',
 						mask: true
 					})
+				}).finally(() => {
+					this.isShow = false;
 				});
-			} else {
-				uni.showModal({
-					title: '提示',
-					content: `无法获取用户信息 ${detail.errMsg}`,
-					showCancel: false,
-					confirmText: '关闭'
-				});
-			}
-		},
-		//获取手机号授权
-		getPhoneNumber(e) {
-			let {
-				detail
-			} = e;
-			if (detail.errMsg == "getPhoneNumber:ok") {
-				this.$emit("success", {
-					meta: detail,
-					scene: 'getPhoneNumber'
-				});
-			} else {
-				uni.showModal({
-					title: '提示',
-					content: `无法获取手机号 ${detail.errMsg}`,
-					showCancel: false,
-					confirmText: '关闭'
-				});
-			}
-		},
-		// 禁止滚动
-		clear() {
-			return;
-		},
 
-	},
-};
+			},
+			//获取手机号授权
+			getPhoneNumber(e) {
+				this.isShow = false;
+				let {
+					detail
+				} = e;
+				if (detail.errMsg == "getPhoneNumber:ok") {
+					this.$emit("success", {
+						meta: detail,
+						scene: 'getPhoneNumber'
+					});
+				} else {
+					uni.showModal({
+						title: '提示',
+						content: `无法获取手机号 ${detail.errMsg}`,
+						showCancel: false,
+						confirmText: '关闭'
+					});
+				}
+
+			},
+			// 禁止滚动
+			clear() {
+				return;
+			},
+
+		},
+	};
 </script>
 
 <style lang="scss">
-.popup_mask {
-	position: fixed;
-	bottom: 0;
-	top: 0;
-	left: 0;
-	right: 0;
-	background-color: rgba(0, 0, 0, 0.4);
-	transition-property: opacity;
-	transition-duration: 0.2s;
-	opacity: 0;
-	z-index: 98;
-}
-
-.popup_mask {
-	opacity: 1;
-}
-
-.popup_content {
-	&.scroll-open {
-		bottom: 0px !important;
+	.popup_mask {
+		position: fixed;
+		bottom: 0;
+		top: 0;
+		left: 0;
+		right: 0;
+		background-color: rgba(0, 0, 0, 0.4);
+		transition-property: opacity;
+		transition-duration: 0.2s;
+		opacity: 0;
+		z-index: 98;
 	}
 
-	&.scroll-animation {
+	.popup_mask {
+		opacity: 1;
+	}
+
+	.popup_content {
+		&.scroll-open {
+			bottom: 0px !important;
+		}
+
+		&.scroll-animation {
+			transition: all 0.25s linear;
+		}
+
 		transition: all 0.25s linear;
-	}
+		overflow: hidden;
+		box-sizing: border-box;
+		padding: 40upx 20upx 0 20upx;
+		position: fixed;
+		// bottom: 30%;
+		bottom: -100%;
+		// bottom: 0;
+		border-top-left-radius: 8px;
+		border-top-right-radius: 8px;
+		// left: 50%;
+		// margin-left: -40%;
+		right: 0;
+		min-height: 400upx;
+		background: #ffffff;
+		width: 100%;
+		z-index: 99;
 
-	transition: all 0.25s linear;
-	overflow: hidden;
-	box-sizing: border-box;
-	padding: 40upx 20upx 0 20upx;
-	position: fixed;
-	// bottom: 30%;
-	bottom: -100%;
-	// bottom: 0;
-	border-top-left-radius: 8px;
-	border-top-right-radius: 8px;
-	// left: 50%;
-	// margin-left: -40%;
-	right: 0;
-	min-height: 400upx;
-	background: #ffffff;
-	width: 100%;
-	z-index: 99;
+		.hd {
+			text-align: left;
+			font-size: 34upx;
+			padding: 10upx 0upx 0 30upx;
+			font-weight: bold;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
 
-	.hd {
-		text-align: left;
-		font-size: 34upx;
-		padding: 10upx 0upx 0 30upx;
-		font-weight: bold;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-
-		.close-icon {
-			color: #c1c1c1;
-			margin-right: 20rpx;
-		}
-	}
-
-	.explain_text {
-		font-size: 13px;
-		padding: 30upx 30upx 40upx 30upx;
-		line-height: 38upx;
-
-		.line {
-			display: block;
-
-			.path {
-				color: #007aff;
-				display: inline-block;
-				text-align: center;
+			.close-icon {
+				color: #c1c1c1;
+				margin-right: 20rpx;
 			}
 		}
-	}
 
-	.button-box {
-		display: flex;
-		padding: 20upx;
-		align-items: center;
-		font-size: 34upx;
-		justify-content: space-around;
-		border-top: 1upx solid #f2f2f2;
-
-		.jw-button {
-			text-align: center;
-			background: #87CE28;
-			color: #fff;
-			border: 1px solid #87CE28;
-			width: 200rpx;
-			height: 80rpx;
-			line-height: 80rpx;
+		.explain_text {
 			font-size: 13px;
+			padding: 30upx 30upx 40upx 30upx;
+			line-height: 38upx;
 
-			.get-phone-number-button {
-				font-size: 34upx;
+			.line {
+				display: block;
+
+				.path {
+					color: #007aff;
+					display: inline-block;
+					text-align: center;
+				}
 			}
+		}
 
-			&.jw-button-cancel {
-				background: #fff;
-				color: #87CE28;
+		.button-box {
+			display: flex;
+			padding: 20upx;
+			align-items: center;
+			font-size: 34upx;
+			justify-content: space-around;
+			border-top: 1upx solid #f2f2f2;
+
+			.jw-button {
+				text-align: center;
+				background: #87CE28;
+				color: #fff;
+				border: 1px solid #87CE28;
+				width: 200rpx;
+				height: 80rpx;
+				line-height: 80rpx;
+				font-size: 13px;
+
+				.get-phone-number-button {
+					font-size: 34upx;
+				}
+
+				&.jw-button-cancel {
+					background: #fff;
+					color: #87CE28;
+				}
 			}
 		}
 	}
-}
 </style>
